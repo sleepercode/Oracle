dd44c0111a61a42183c4f2bc8c4209248da696c7
Amel <avdic.amel@gmail.com>
2023-03-15 23:52:57 +0100

    Create Oracle_apex_infobip_viber_fallback_sms.sql

---
+++ Oracle_apex_infobip_viber_fallback_sms.sql
@@ -0,0 +1,60 @@
+DECLARE
+  l_response CLOB;
+  l_url VARCHAR2(4000);
+  l_headers apex_web_service.t_param_list;
+  l_body VARCHAR2(4000) := '{"messages":[{"content":{"text":"Hello, this is a Viber message!"},"destination":{"to":"{phone_number}","country":"{country_code}"}}]}';
+  l_max_attempts NUMBER := 3;
+  l_attempt NUMBER := 1;
+  l_success BOOLEAN := FALSE;
+BEGIN
+  l_headers := apex_web_service.g_request_headers;
+  apex_web_service.set_header(l_headers, 'Authorization', 'App {api_key}');
+  apex_web_service.set_header(l_headers, 'Content-Type', 'application/json');
+  
+  -- Try sending Viber message
+  WHILE NOT l_success AND l_attempt <= l_max_attempts LOOP
+    l_response := apex_web_service.make_rest_request(
+      p_url => 'https://api.infobip.com/viber/1/send',
+      p_http_method => 'POST',
+      p_parm_name => apex_util.string_to_table('p_body'),
+      p_parm_value => apex_util.string_to_table(l_body),
+      p_header => l_headers
+    );
+    
+    IF apex_web_service.get_status_code >= 200 AND apex_web_service.get_status_code < 300 THEN
+      -- Viber message was sent successfully
+      l_success := TRUE;
+    ELSE
+      -- Viber message failed to send, retry or fallback to SMS
+      l_attempt := l_attempt + 1;
+      
+      IF l_attempt <= l_max_attempts THEN
+        -- Wait for a few seconds before retrying
+        DBMS_LOCK.SLEEP(5);
+      ELSE
+        -- Fallback to secondary SMS API
+        l_url := 'https://api.example.com/sms/send';
+        l_body := '{"message":"Hello, this is an SMS fallback message!","phone_number":"{phone_number}"}';
+        l_response := apex_web_service.make_rest_request(
+          p_url => l_url,
+          p_http_method => 'POST',
+          p_parm_name => apex_util.string_to_table('p_body'),
+          p_parm_value => apex_util.string_to_table(l_body)
+        );
+        
+        IF apex_web_service.get_status_code >= 200 AND apex_web_service.get_status_code < 300 THEN
+          -- SMS message was sent successfully
+          l_success := TRUE;
+        ELSE
+          -- SMS message failed to send after max attempts
+          DBMS_OUTPUT.PUT_LINE('SMS message failed to send after ' || l_max_attempts || ' attempts');
+        END IF;
+      END IF;
+    END IF;
+  END LOOP;
+  
+  IF NOT l_success THEN
+    -- Viber message and SMS message both failed to send after max attempts
+    DBMS_OUTPUT.PUT_LINE('Sending Viber message and fallback SMS message both failed after ' || l_max_attempts || ' attempts');
+  END IF;
+END;
